373fbe656d1be19cef53a002c85b8a63d0aa5977
mamd0uh <mamdouh.elmalah@gmail.com>
2022-11-17 06:17:31 -0500

    updated frontend app and index files

---
+++ frontend/public/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/users/Login';
+import Register from './pages/users/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
