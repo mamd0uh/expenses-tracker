288b8bed5bb54c85bd8acaf38726ad1a549643df
mamd0uh <mamdouh.elmalah@gmail.com>
2022-11-17 06:16:08 -0500

    added user routes

---
+++ backend/src/routes/users/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { registerUser, fetchUsersCtrl, loginUserCtrl } = require('../../controllers/users/usersCtrl');
+const userRoute = express.Router()
+
+userRoute.post('/register', registerUser)
+userRoute.post('/login', loginUserCtrl)
+userRoute.get('/', fetchUsersCtrl)
+
+module.exports = userRoute
